cc6a6bd8e269825b2b2491b471cf90a5bd22d6c3
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-02-02 17:09:29 +0900

    Search 컴포넌트와 Button 공통 스타일

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,17 @@
 import { Component } from "../core/modules";
 import Headline from "../components/Headline";
+import Search from "../components/Search";
 
 export default class Home extends Component {
   render() {
-    const headeline = new Headline().el; // 요소로 만들기
+    // 요소로 만들기
+    const headeline = new Headline().el;
+    const search = new Search().el;
+
+    // Home 내부의 요소에 container 클래스 추가하기
     this.el.classList.add("container");
-    this.el.append(headeline);
+
+    // 요소로 만든 js파일 container 내부에 출력하기
+    this.el.append(headeline, search);
   }
 }
