08a20adf8bd3774ce371019bf3bde98868be5dc5
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-02-06 15:32:28 +0900

    added TheHeader 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import TheHeader from "./components/TheHeader";
 import { Component } from "./core/modules";
 
 export default class App extends Component {
   render() {
     const routerView = document.createElement("router-view");
-
-    this.el.append(routerView);
+    const theHeader = new TheHeader().el;
+    this.el.append(theHeader, routerView);
   }
 }
