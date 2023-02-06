4a8a1576d1340301aaf2f8a9da68241d7e65dab9
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-02-06 17:32:24 +0900

    modify names

--- src/store/about.js
+++ src/store/about.js
@@ -3,7 +3,7 @@ import { Store } from "../core/modules";
 export default new Store({
   photo:
     "https://i.pinimg.com/564x/53/6c/a3/536ca3eb5f5e1c497acb6b120c870191.jpg",
-  name: "ANNE / Jang A Reum",
+  name: "JANG A REUM",
   email: "alliejj99@gmail.com",
   blog: "https://www.notion.so/Jang-A-Reum-a02309618af04592a862aa2467c95722",
   github: "https://github.com/alliejj99",
