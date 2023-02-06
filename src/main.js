ae2da7c3e751f379cad79fe071ec63d306c64672
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-02-06 17:49:52 +0900

    vercel 구성 및 serverless functions 테스트

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import App from "./App";
 const root = document.querySelector("#root"); // 하위 파일 내용 넣을 태그 선택하기
 root.append(new App().el); // 하위 파일 불러오기
 router(); // 페이지를 분리할 수 있는 라우터 실행 코드
+
+(async () => {
+  const res = await fetch("/api/test");
+  const json = await res.json();
+  console.log(json);
+})();
